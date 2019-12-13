07e2a119cbbad4c0a95b1c04912699edafe91740
Antwan Maddox <antwan.maddox@agilethought.com>
2019-12-13 14:39:55 -0500

    initial commit

---
+++ Nunit_Parallel/TestClass.cs
@@ -0,0 +1,45 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Nunit_Parallel
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public IWebDriver _driver;
+
+        [Test]
+        [Parallelizable]
+        public void TestMethod1()
+        {
+            _driver.Navigate().GoToUrl("http://www.espn.com");
+           
+        }
+
+        [Test]
+        [Parallelizable]
+        public void TestMethod2()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            //Also try to replace driver.close() with driver.quit();
+            _driver.Quit();
+        }
+
+    }
+}
